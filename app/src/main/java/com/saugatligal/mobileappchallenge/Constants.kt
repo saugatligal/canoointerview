31248e305c586365f95a36eeadd4b1fd2f41dbc0
saugatligal <saugatligal@gmail.com>
2023-03-26 20:56:19 -0500

    added screenshots for readme file

--- app/src/main/java/com/saugatligal/mobileappchallenge/Constants.kt
+++ app/src/main/java/com/saugatligal/mobileappchallenge/Constants.kt
@@ -7,7 +7,7 @@ object Constants {
     const val HOTEL = "HOTEL"
     const val ERROR_MESSAGE = "Please, try again later"
     const val NO_INTERNET_MESSAGE = "No internet Connection"
-    const val API_KEY = "726b2a969amsh81205a8bffc9ff2p10c9d4jsn97ec1a49c006"
+    const val API_KEY = "X-RapidAPI-Key: 726b2a969amsh81205a8bffc9ff2p10c9d4jsn97ec1a49c006"
     const val API_HOST = "X-RapidAPI-Host: hotels4.p.rapidapi.com"
 
 
